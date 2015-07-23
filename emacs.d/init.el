a3ab175cf84661ddeab64077994e5e245e35fb6a
Peter Braam <peter.braam@peterbraam.com>
2015-07-23 14:44:26 -0600

    changes for init.el

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -22,14 +22,20 @@
 (require 'haskell-mode-autoloads)
 (add-to-list 'Info-default-directory-list "~/.emacs.d/packages/haskell-mode/")
 
+;; setup files ending in “.lg” to open in lua-mode
+(add-to-list 'auto-mode-alist '("\\.lg\\'" . lua-mode))
+
+
+
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(ansi-color-names-vector ["#212526" "#ff4b4b" "#b4fa70" "#fce94f" "#729fcf" "#ad7fa8" "#8cc4ff" "#eeeeec"])
  '(column-number-mode t)
- '(custom-enabled-themes (quote (deeper-blue)))
+ '(custom-enabled-themes (quote (tsdh-dark)))
  '(inhibit-startup-screen t)
  '(show-paren-mode t)
  '(text-mode-hook (quote (turn-on-auto-fill text-mode-hook-identify)))
