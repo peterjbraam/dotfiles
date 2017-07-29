0eb0905d8bb14c8bb11b006fcb1cffb0c8595929
peterjbraam <peter.braam@campaignstorage.com>
2017-07-29 09:04:35 -0600

    commit to transfer to new laptop

---
+++ emacs.d/SessionDesktop.el
@@ -0,0 +1,58 @@
+;; -*- mode: emacs-lisp; coding: emacs-mule; -*-
+;; --------------------------------------------------------------------------
+;; Desktop File for Emacs
+;; --------------------------------------------------------------------------
+;; Created Fri Jun 30 21:29:55 2017
+;; Desktop file format version 208
+;; Emacs version 25.1.1
+(setq revive:frame-configuration-to-restore
+  '(progn 
+     (revive:restore-frame '((tool-bar-position . top) (explicit-name) (icon-name) (bottom-divider-width . 0) (right-divider-width . 0) (top . 23) (left . 0) (unsplittable) (modeline . t) (width . 101) (height . 49) (fringe) (mouse-color . "black") (environment) (visibility . t) (cursor-color . "Red") (background-mode . light) (fullscreen) (alpha) (scroll-bar-height . 0) (scroll-bar-width . 15) (cursor-type . box) (auto-lower) (auto-raise) (icon-type) (title) (buffer-predicate) (tool-bar-lines . 1) (menu-bar-lines . 1) (right-fringe . 8) (left-fringe . 1) (line-spacing) (background-color . "#ffffff") (foreground-color . "Black") (horizontal-scroll-bars) (vertical-scroll-bars . right) (internal-border-width . 0) (border-width . 0) (font . "-*-Monaco-normal-normal-normal-*-14-*-*-*-m-0-iso10646-1") (fontsize . 0)) '(101 49 ((0 0 104 48)) ((nil "*Customize Group: Aquamacs Is More Than Emacs*" 1 1 ((tab "*scratch*" "/Users/braam/Library/Application Support/Aquamacs Emacs/scratch buffer") (tab "txt" "/Users/braam/programming/txt") (tab "marfs_ops.c" "/Users/braam/programming/marfs/fuse/src/marfs_ops.c"))))))))
+
+;; Global section:
+(setq desktop-saved-frameset [frameset 1 (22871 5939 450881 0) (desktop . "208") "braam@peters-mbp-3.lan" nil nil ((((font-backend mac-ct) (fontsize . 0) (font . "-*-Monaco-normal-normal-normal-*-14-*-*-*-m-0-iso10646-1") (border-width . 0) (internal-border-width . 0) (vertical-scroll-bars . right) (horizontal-scroll-bars) (foreground-color . "Black") (background-color . "#ffffff") (line-spacing) (left-fringe . 1) (right-fringe . 8) (menu-bar-lines . 1) (tool-bar-lines . 1) (title) (icon-type) (auto-raise) (auto-lower) (cursor-type . box) (scroll-bar-width . 15) (scroll-bar-height . 0) (alpha) (fullscreen) (display-type . color) (background-mode . light) (cursor-color . "Red") (visibility . t) (environment) (mouse-color . "black") (fringe) (frameset--id . "E954-08CE-25C6-9F3F") (frameset--mini t . t) (height . 49) (width . 101) (modeline . t) (minibuffer . t) (unsplittable) (left . 0) (top . 23) (right-divider-width . 0) (bottom-divider-width . 0) (icon-name) (display . "peters-mbp-3.lan") (explicit-name) (tool-bar-position . top)) ((min-height . 4) (min-width . 10) (min-height-ignore . 3) (min-width-ignore . 5) (min-height-safe . 1) (min-width-safe . 2) (min-pixel-height . 76) (min-pixel-width . 80) (min-pixel-height-ignore . 56) (min-pixel-width-ignore . 40) (min-pixel-height-safe . 19) (min-pixel-width-safe . 16)) leaf (pixel-width . 832) (pixel-height . 912) (total-width . 104) (total-height . 48) (normal-height . 1.0) (normal-width . 1.0) (buffer "*Customize Group: Aquamacs Is More Than Emacs*" (selected . t) (hscroll . 0) (fringes 1 8 nil) (margins nil) (scroll-bars nil 2 t nil 0 nil) (vscroll . 0) (dedicated) (point . 1) (start . 1))))])
+(setq desktop-missing-file-warning nil)
+(setq tags-file-name nil)
+(setq tags-table-list nil)
+(setq search-ring nil)
+(setq regexp-search-ring nil)
+(setq register-alist nil)
+(setq file-name-history '("~/programming/txt" "~/programming/marfs/fuse/src/marfs_ops.c" "~/programming/txt" "~/programming/marfs/fuse/src/marfs_ops.c"))
+
+;; Buffer section -- buffers listed in same order as in buffer list:
+(desktop-create-buffer 208
+  "/Users/braam/programming/txt"
+  "txt"
+  'fundamental-mode
+  nil
+  5
+  '(nil nil)
+  nil
+  nil
+  '((buffer-file-coding-system . utf-8-unix))
+  '((mark-ring nil)))
+
+(desktop-create-buffer 208
+  "/Users/braam/programming/marfs/fuse/src/marfs_ops.c"
+  "marfs_ops.c"
+  'c-mode
+  '(abbrev-mode)
+  17740
+  '(nil nil)
+  nil
+  nil
+  '((buffer-file-coding-system . utf-8-unix))
+  '((mark-ring nil)))
+
+(desktop-create-buffer 208
+  "/Users/braam/Library/Application Support/Aquamacs Emacs/scratch buffer"
+  "*scratch*"
+  'text-mode
+  '(visual-line-mode smart-spacing-mode)
+  1
+  '(nil nil)
+  nil
+  nil
+  '((truncate-lines))
+  '((mark-ring nil)))
+
